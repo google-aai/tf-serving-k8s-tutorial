7ff27b855278d41812e7c5fec38a82b91c5b27fe
Brian Foo <bfoo@google.com>
2018-08-30 15:31:50 -0700

    Remove script to install docker and add kubeflow deploy script
    deploy_tf_serving.sh is a minimal deploy script that generates a
    ksonnet app for only tf model server. The full script can be installed
    via kubeflow.org

---
+++ gcp/deploy_tf_serving.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# This script creates a kubeflow deployment on GCP
+# It checks for kubectl, gcloud, ks
+# Uses default PROJECT, ZONE, EMAIL from gcloud config
+# Creates a deployment manager config copy and edits appropriate values
+# Creates the ksonnet app, installs packages, components and then applies them
+#
+
+# Go to your home directory
+cd ~
+
+# Install Kubeflow
+KUBEFLOW_VERSION=0.2.2
+KUBEFLOW_REPO=`pwd`/kubeflow_repo
+KUBEFLOW_DEPLOY=true
+KUBEFLOW_CLOUD="gke"
+DEPLOYMENT_NAME=tf_serving
+
+TAG=v${KUBEFLOW_VERSION}
+TMPDIR=$(mktemp -d /tmp/tmp.kubeflow-repo-XXXX)
+curl -L -o ${TMPDIR}/kubeflow.tar.gz https://github.com/kubeflow/kubeflow/archive/${TAG}.tar.gz
+tar -xzvf ${TMPDIR}/kubeflow.tar.gz  -C ${TMPDIR}
+# GitHub seems to strip out the v in the file name.
+SOURCE_DIR=$(find ${TMPDIR} -maxdepth 1 -type d -name "kubeflow*")
+mv ${SOURCE_DIR} "${KUBEFLOW_REPO}"
+
+source "${KUBEFLOW_REPO}/scripts/util.sh"
+
+# Check all requirements
+check_install gcloud
+check_install kubectl
+check_install ks
+
+KUBEFLOW_KS_DIR=`pwd`/${DEPLOYMENT_NAME}_ks_app
+
+# Namespace where kubeflow is deployed. Just use default namespace to reduce confusion for students.
+K8S_NAMESPACE=default
+
+# Create the ksonnet app
+cd $(dirname "${KUBEFLOW_KS_DIR}")
+ks init $(basename "${KUBEFLOW_KS_DIR}")
+cd "${KUBEFLOW_KS_DIR}"
+
+ks env set default --namespace "${K8S_NAMESPACE}"
+# Add the local registry
+ks registry add kubeflow "${KUBEFLOW_REPO}/kubeflow"
+
+# Install all required packages
+ks pkg install kubeflow/tf-serving
+
+# Generate all required components
+
+ks generate tf-serving tf-serving
+
