5bdfa6819b596b5ffd842b8121fd3173419be300
Brian Foo <bfoo@google.com>
2018-09-07 23:22:35 -0700

    Remove jupyter installation from client vm
    Since all Jupyter code will be run in JupyterHub,
    remove all Jupyter installation requirements.

--- gcp/setup_client_vm.sh
+++ gcp/setup_client_vm.sh
@@ -29,8 +29,6 @@ err() {
 #### Bash variables ####
 # The directory of the shell script, e.g. ~/code in the lecture slides.
 BASE_DIR=$(dirname "$0")
-# The remote port for Jupyter traffic.
-PORT=8888
 
 #### APT-GET library installations ####
 
@@ -50,7 +48,7 @@ sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
 
 # Create a virtual environment.
-virtualenv -p python3 $HOME/env
+virtualenv -p python2 $HOME/env
 
 # Create a function to activate environment in shell script.
 activate () {
@@ -64,32 +62,8 @@ sed -i '/source.*\/bin\/activate/d' $HOME/.bashrc
 echo 'source $HOME/env/bin/activate' >> $HOME/.bashrc
 
 # Install requirements in the virtualenv
-pip install -r $BASE_DIR/../jupyter_requirements.txt \
+pip install -r $BASE_DIR/../client_requirements.txt \
   || err 'failed to pip install a required library'
-
-#### JUPYTER SETUP ####
-
-# Switch into $BASE_DIR, e.g. ~/code
-cd $BASE_DIR
-
-# Create a config file for jupyter. Defaults to location ~/.jupyter/jupyter_notebook_config.py
-jupyter notebook --generate-config
-
-# Append several lines to the config file
-echo "c = get_config()" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.port = $PORT" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.allow_origin = '*'" >> ~/.jupyter/jupyter_notebook_config.py
-
-# Create a password for jupyter. This is necessary for remote web logins.
-# The password will be hashed and written into ~/.jupyter/jupyter_notebook_config.json
-jupyter notebook password
-
-# Hacky way to parse the json to pick up the hashed password and add to config file
-PASSWORD_HASH=$(cat ~/.jupyter/jupyter_notebook_config.json | grep password | cut -d"\"" -f4)
-echo "c.NotebookApp.password = u'$PASSWORD_HASH'" >> ~/.jupyter/jupyter_notebook_config.py
-echo "Done with jupyter setup!"
 
 # Add some env variables into your bashrc
 echo 'Done with installation! Make sure to type: . ~/.bashrc to finish setup.'
